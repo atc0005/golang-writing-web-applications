946a2843e7bed6df5e4d99d4f54cc71a8c85cebc
Adam Chalkley <atc0005@users.noreply.github.com>
2020-01-28 07:01:05 -0600

    Use templated output for "/view/" and "/edit/"
    Not quite done with "part3" yet, but close.

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
 // Page describes how page data will be stored in memory.
@@ -30,7 +30,8 @@ func loadPage(title string) (*Page, error) {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	p, _ := loadPage(title)
-	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,12 +44,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 		p = &Page{Title: title}
 	}
 
-	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		"<form action=\"/save/%s\" method=\"POST\">"+
-		"<textarea name=\"body\">%s</textarea><br>"+
-		"<input type=\"submit\" value=\"Save\""+
-		"</form>",
-		p.Title, p.Title, p.Body)
+	// read the contents of edit.html and return a *template.Template object
+	t, _ := template.ParseFiles("edit.html")
+
+	// execute the template, writing the generated HTML to the
+	// http.ResponseWriter. The .Title and .Body dotted identifiers in the
+	// template file refer to p.Title and p.Body.
+	t.Execute(w, p)
 }
 
 func main() {
