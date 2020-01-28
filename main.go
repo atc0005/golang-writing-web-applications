7e0cd4f046b4a466ddcb9baa9daa844948ffe81b
Adam Chalkley <atc0005@users.noreply.github.com>
2020-01-28 05:53:00 -0600

    Implement editHandler, stub out saveHandler funcs
    This is where we are usin Fprintf() to generate the HTML
    output instead of using html/template.

--- main.go
+++ main.go
@@ -33,7 +33,36 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+
+	// chop off "/edit/" from the URL and use what is left as the page title
+	title := r.URL.Path[len("/edit/"):]
+
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\""+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 func main() {
+
+	// loads the page, displays edit form for new page if not existing page
 	http.HandleFunc("/view/", viewHandler)
+
+	// displays edit form for existing page, otherwise edit form for new page
+	http.HandleFunc("/edit/", editHandler)
+
+	// save the data entered into the edit form
+	// TODO
+	//http.HandleFunc("/save/", saveHandler)
+
+	// listen on port 8080 on any interface, block until app is terminated
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
